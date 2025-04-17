e9c9cffc37289ec793624cce8cc661bbfc077c17
jampierzcode <jampierv127@gmail.com>
2025-04-17 10:13:10 -0500

    spin cargado

--- src/components/rolRepartidor/ImageUploadModal.js
+++ src/components/rolRepartidor/ImageUploadModal.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from "react";
-import { Modal, Button } from "antd";
+import { Modal, Button, Spin } from "antd";
 import {
   UploadOutlined,
   CameraOutlined,
